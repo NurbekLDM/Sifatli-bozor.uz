73c1db86cadb41381a124449849b0f752ed2885e
NurbekLDM <jumaboyevabarno80@gmail.com>
2024-12-24 21:48:45 +0500

    Initial comit

--- products/urls.py
+++ products/urls.py
@@ -6,7 +6,6 @@ from django.contrib import admin
 urlpatterns = [
     path('', views.product_list, name='product_list'),
     path('product/<int:pk>/', views.product_detail, name='product_detail'),
-    path('admin-panel/', views.admin_panel, name='admin_panel'),
     path('admin-panel/product/add/', views.product_add, name='product_add'),
     path('admin-panel/product/<int:pk>/edit/', views.product_edit, name='product_edit'),
     path('admin-panel/product/<int:pk>/delete/', views.product_delete, name='product_delete'),
@@ -14,5 +13,5 @@ urlpatterns = [
     path('admin-panel/category/<int:pk>/edit/', views.category_edit, name='category_edit'),
     path('admin-panel/category/<int:pk>/delete/', views.category_delete, name='category_delete'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
-  path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls, name='admin_panel'),
 ]
