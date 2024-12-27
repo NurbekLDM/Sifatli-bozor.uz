3339b99771e89c2ce883d64f3d10a184c0e6f5b1
Shaxzodbek <muxtorovshaxzodbek16@gmail.com>
2024-12-27 19:44:04 +0500

    ready to deploy

--- runner.sh
+++ runner.sh
@@ -2,13 +2,23 @@
 
 black .
 
+sleep 1
+
 pip freeze > requirements.txt
+
+sleep 1
 
 python manage.py makemigrations
 
+sleep 1
+
 python manage.py migrate
 
+sleep 1
+
 python manage.py test
+
+sleep 1
 
 python manage.py runserver
 
