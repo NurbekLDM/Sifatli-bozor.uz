747563d55187ae0854344d75c21d7816291d8452
Shaxzodbek <muxtorovshaxzodbek16@gmail.com>
2024-12-27 23:20:26 +0500

    some changes

--- project/settings.py
+++ project/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG") == "True"
+DEBUG = os.getenv("DEBUG", default=False)
 
 
 # ALLOWED_HOSTS = [host for host in os.getenv("ALLOWED_HOSTS").split(",") if host]
